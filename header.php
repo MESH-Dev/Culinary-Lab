73e9243898e0469d696eb3f64985822c3b23e162
robbiesh <sherrard.rob@gmail.com>
2016-06-02 13:50:31 -0400

    img fix thing

--- header.php
+++ header.php
@@ -60,7 +60,7 @@
 			<?php } ?>
 
 			<nav class="main-navigation">
-				<div class="close"><img src="<?php echo get_template_directory_uri(); ?>/img/sidr-close.png ?>" ></div>
+				<div class="close"><img src="<?php echo get_template_directory_uri(); ?>/img/sidr-close.png"></div>
 				<?php if(has_nav_menu('main_nav')){
 							$defaults = array(
 								'theme_location'  => 'main_nav',
@@ -85,7 +85,7 @@
 						} ?>
 			</nav>
 
-			<div class="nav_trigger hide"><img src="<?php echo get_template_directory_uri(); ?>/img/nav_trigger.png ?>" ></div>
+			<div class="nav_trigger hide"><img src="<?php echo get_template_directory_uri(); ?>/img/nav_trigger.png" ></div>
 
 
 		</header>
